caf9b9ca6d9da07352481bcc10e9d52ae74ad2ba
hemantbytes <https://github.com/hemantbytes/React-Native.git>
2025-01-15 18:10:50 +0530

    Add all untracked files

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,17 @@
 const mongoose = require('mongoose');
-const mongoURI = "mongodb://localhost:27017/?directConnection=true"
-const connectToMongo = () => {
-    mongoose.connect(mongoURI, () => {
+const mongoURI = "mongodb://127.0.0.1:27017/inotebook";
+
+const connectToMongo = async () => {
+    try {
+        await mongoose.connect(mongoURI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
         console.log("Connected to Mongo Successfully");
-    })
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error.message);
+        // You can choose to handle the error in a way that suits your application.
+    }
 }
+
 module.exports = connectToMongo;
